edab9503b72bf2540c0a96e91ee3b9bc754a3ab8
Nobuyuki Iwanaga <ufcpp@live.jp>
2022-08-30 16:49:27 +0900

    TemplateClassMember

--- src/TextTemplateSourceGenerator/TextTemplateGenerator.Init.cs
+++ src/TextTemplateSourceGenerator/TextTemplateGenerator.Init.cs
@@ -15,14 +15,28 @@ partial class TextTemplateGenerator
             ExperimentalA,
         }
 
-        [System.Diagnostics.Conditional("COMPILE_TIME_ONLY")]
+        /// <summary>
+        /// Generates a template preprocessor method.
+        /// </summary>
+        [System.Diagnostics.Conditional("TEXT_TEMPLATE_COMPILE_TIME_ONLY")]
         [AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = false)]
         internal sealed class TemplatePreprocessorAttribute : Attribute
         {
             public TemplateLanguage Language { get; set; }
             public TemplatePreprocessorAttribute(string template, string appendMethodName = null) { }
         }
-
+        
+        /// <summary>
+        /// Generates class members from a template.
+        /// </summary>
+        [System.Diagnostics.Conditional("TEXT_TEMPLATE_COMPILE_TIME_ONLY")]
+        [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+        internal sealed class TemplateClassMemberAttribute : Attribute
+        {
+            public TemplateLanguage Language { get; set; }
+            public TemplateClassMemberAttribute(string template) { }
+        }
+        
         """;
     private static void AddAttribute(IncrementalGeneratorPostInitializationContext context)
     {
